3f03dc9ea1ad90d7258439562a268be636d669d5
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-06-20 19:47:26 +0330

    P1 Chaos

---
+++ CSharpCourse/S004.FormatDocument/P1.Chaos/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P1.Chaos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("AB");
+            Console.Write("CD");
+
+
+
+            Console.Write("EF"); Console.Write("GH"); Console.Write("IJ");
+
+    Console.Write("KL");
+                                        Console.Write("MN");
+
+
+            Console.Write("OP");
+        }
+    }
+}
