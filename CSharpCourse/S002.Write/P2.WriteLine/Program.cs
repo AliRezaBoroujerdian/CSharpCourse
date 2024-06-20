6ed48cc21a6c31777bd815abd17e3a7426bbb0de
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-06-20 19:17:47 +0330

    P2 WriteLine

---
+++ CSharpCourse/S002.Write/P2.WriteLine/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P2.WriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test 1"); 
+
+            Console.WriteLine("Test 2");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Test 3");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Test 4");
+        }
+    }
+}
