46be82009c8e5798c7982ce2b9785d64242e7c06
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-06-20 19:52:16 +0330

    P2 Format

---
+++ CSharpCourse/S004.FormatDocument/P2.Format/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P2.Format
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shortcut
+            // Ctrl+K, Ctrl+D
+
+            Console.Write("AB");
+            Console.Write("CD");
+
+
+
+            Console.Write("EF"); Console.Write("GH"); Console.Write("IJ");
+
+            Console.Write("KL");
+            Console.Write("MN");
+
+
+            Console.Write("OP");
+        }
+    }
+}
