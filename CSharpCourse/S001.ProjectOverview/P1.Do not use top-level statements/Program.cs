d16c001f7c8b7d86499c70c2ef83e008e5c91048
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-06-20 16:31:02 +0330

    Project Overview

---
+++ CSharpCourse/S001.ProjectOverview/P1.Do not use top-level statements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P1.Do_not_use_top_level_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
