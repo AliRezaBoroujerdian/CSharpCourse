592169afc54369ffa28f379df9f192af47ddb6b5
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-06-20 19:44:49 +0330

    P1 Comment

---
+++ CSharpCourse/S003.Comment/P1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a comment
+
+            Console.WriteLine("You can see this on the screen while the project is running!");
+
+            //Console.WriteLine("You can not see this on the screen while the project is running!");
+
+            Console.WriteLine("This is another line"); // This is another comment.
+
+            /* The code below will print the words Hello World
+            to the screen, and it is amazing */
+            Console.WriteLine("Hello World!");
+
+            /*
+             * 
+             * cm line 1
+             * cm line 2
+             * 
+             * 
+             * cm line 3
+             * 
+             * 
+             * 
+             */
+
+
+        }
+    }
+}
