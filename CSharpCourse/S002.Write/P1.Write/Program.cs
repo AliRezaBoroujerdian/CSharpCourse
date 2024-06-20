215358913e0c8773457843ca96fb3db9f10632c3
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-06-20 19:09:59 +0330

    P1 Write

---
+++ CSharpCourse/S002.Write/P1.Write/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P1.Write
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Test 1");
+
+            Console.Write("Test 2       ");
+
+            Console.Write("Test 3");
+
+            Console.Write(" - - - Test 4");
+        }
+    }
+}
